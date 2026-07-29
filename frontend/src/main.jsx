9b070a8b6770f0a12d08472c976aac73df844e6f
Dilushan.M <dilushanmehavannan@gmail.com>
2026-07-29 21:27:43 +0530

    Use dynamic VITE_GOOGLE_CLIENT_ID environment variable in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx';
 import './index.css';
 
-const GOOGLE_ID = '432702684432-pe2lqnde7l3cucfu424hcfubqorgfjoi.apps.googleusercontent.com';
+const GOOGLE_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '432702684432-pe2lqnde7l3cucfu424hcfubqorgfjoi.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
